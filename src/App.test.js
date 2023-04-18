eb0b9c2a90a315c1836ac4e89f9db71eee5e4517
The Swiss Bell <TheSwissBell@outlook.com>
2023-04-18 14:54:23 +0300

    added 2 tests (renders todotable and add todo

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,29 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import '@testing-library/jest-dom/extend-expect';
+import TodoTable from './TodoTable';
+import { fireEvent } from '@testing-library/react';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders todotable', () => {
+  const row = [
+    {
+      desc: 'Go to coffee',
+      date: '24.01.2021'
+    }
+  ];
+  render(< TodoTable todos={row} />);
+  const tablecell = screen.getByText(/go to coffee/i);
+  expect(tablecell).toBeInTheDocument();
 });
+
+test('add todo', () => {
+  render(<App />);
+  const desc = screen.getByPlaceholderText('Description');
+  fireEvent.change(desc, { target: { value: 'Go to coffee' } });
+  const date = screen.getByPlaceholderText('Date');
+  fireEvent.change(date, { target: { value: '29.01.2021' } })
+  const button = screen.getByText('Add');
+  fireEvent.click(button);
+  const tablecell = screen.getByText(/go to coffee/i);
+  expect(tablecell).toBeInTheDocument();
+})
