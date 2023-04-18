8c383751f4866aa0ff6dbdb09332d2cfe5aa4649
The Swiss Bell <TheSwissBell@outlook.com>
2023-04-18 15:14:34 +0300

    added test case for clear functionality

--- src/App.test.js
+++ src/App.test.js
@@ -27,3 +27,31 @@ test('add todo', () => {
   const tablecell = screen.getByText(/go to coffee/i);
   expect(tablecell).toBeInTheDocument();
 })
+
+test('clear todos', () => {
+  const res = render(<App />);
+  const table = res.container.querySelector('table');
+  const desc = screen.getByPlaceholderText('Description');
+  fireEvent.change(desc, { target: { value: 'Go to coffee' } });
+  const date = screen.getByPlaceholderText('Date');
+  fireEvent.change(date, { target: { value: '18.04.2023' } })
+  const button = screen.getByText('Add');
+  fireEvent.click(button);
+  fireEvent.change(desc, { target: { value: 'Go to tea' } });
+  fireEvent.change(date, { target: { value: '22.04.2023' } })
+  fireEvent.click(button);
+
+  expect(table.rows.length).toBe(2);
+
+  const tablecell = screen.getByText(/go to tea/i);
+  expect(tablecell).toBeInTheDocument();
+
+  const clearButton = screen.getByText(/Clear/i);
+  fireEvent.click(clearButton);
+
+  expect(table.rows.length).toBe(0);
+  expect(tablecell).not.toBeInTheDocument();
+  
+
+
+})
